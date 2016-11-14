0a939a90d388ab67ec489f28bec8d42946735a9e
Tucker Bamberg <mootpoint23@gmail.com>
2016-11-14 14:05:05 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,5 +12,6 @@
 ]]
 
 fishing = {}
-dofile(minetest.get_modpath("fishing") .. '/craft.lua' )
-dofile(minetest.get_modpath("fishing") .. '/sealife.lua' )
+dofile(minetest.get_modpath('fishing') .. '/craft.lua' )
+dofile(minetest.get_modpath('fishing") .. '/sealife.lua' ) 
+dofile(minetest.get_modpath('fishing") .. '/conversion.lua' )
