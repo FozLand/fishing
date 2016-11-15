51e13bf51b736099a322a073cf3b42b1a3628a14
Louis Chartrand <mokawi@gmail.com>
2016-11-14 23:56:00 -0500

    Fixed mismatched quotes

--- init.lua
+++ init.lua
@@ -13,5 +13,5 @@
 
 fishing = {}
 dofile(minetest.get_modpath('fishing') .. '/craft.lua' )
-dofile(minetest.get_modpath('fishing") .. '/sealife.lua' ) 
-dofile(minetest.get_modpath('fishing") .. '/conversion.lua' )
+dofile(minetest.get_modpath('fishing') .. '/sealife.lua' ) 
+dofile(minetest.get_modpath('fishing') .. '/conversion.lua' )
