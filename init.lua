0700c0f5c5ab0639c0ce0d8f651eda2cacb091b6
mokawi <mokawi@gmail.com>
2016-11-14 23:54:37 -0500

    Added conversion.lua to init.lua

--- init.lua
+++ init.lua
@@ -14,3 +14,5 @@
 fishing = {}
 dofile(minetest.get_modpath("fishing") .. '/craft.lua' )
 dofile(minetest.get_modpath("fishing") .. '/sealife.lua' )
+dofile(minetest.get_modpath"'fishing") .. '/conversion.lua' )
+
