4b44a5c377c193509700b9ce1de7b848f4fa9039
Tucker Bamberg <mootpoint23@gmail.com>
2016-11-15 10:27:22 -0500

    Added Parenetheses that cause crash at start
    Also minor formatting

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@
 ]]
 
 fishing = {}
-dofile(minetest.get_modpath("fishing") .. '/craft.lua' )
-dofile(minetest.get_modpath("fishing") .. '/sealife.lua' )
-dofile(minetest.get_modpath"'fishing") .. '/conversion.lua' )
+dofile(minetest.get_modpath('fishing') .. '/craft.lua' )
+dofile(minetest.get_modpath('fishing') .. '/sealife.lua' )
+dofile(minetest.get_modpath('fishing') .. '/conversion.lua' )
 
