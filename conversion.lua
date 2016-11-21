c7445de8b18e6c1acdabe50d4fe59ce2afd31d2b
Foz <fozolo@gmail.com>
2016-11-20 15:33:19 -1000

    Add alias for fishing:sandy.

--- conversion.lua
+++ conversion.lua
@@ -4,6 +4,8 @@ minetest.register_lbm({
 	name = 'fishing:remove_sandy',
 	nodenames = {'fishing:sandy'},
 	action = function(pos, node)
-		minetest.set_node(pos, {name = "default:sand"})
+		minetest.set_node(pos, {name = 'default:sand'})
 	end,
 })
+
+minetest.register_alias('fishing:sandy', 'default:sand')
